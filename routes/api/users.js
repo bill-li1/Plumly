03e7b75f43e2ed88b7b0dde8d1ae3280d62a6429
Bill Li <billli@gmail.com>
2021-02-18 16:10:29 -0500

    finished backend, starting react

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,18 +11,16 @@ const normalize = require("normalize-url");
 const User = require("../../models/User");
 
 // @route    POST api/users
-// @desc     register route
-// @access   public
+// @desc     Register user
+// @access   Public
 router.post(
 	"/",
-	[
-		check("name", "Name is required").not().isEmpty(),
-		check("email", "Please enter a valid email").isEmail(),
-		check(
-			"password",
-			"Please enter a password with 6 or more characters"
-		).isLength({ min: 6 }),
-	],
+	check("name", "Name is required").notEmpty(),
+	check("email", "Please include a valid email").isEmail(),
+	check(
+		"password",
+		"Please enter a password with 6 or more characters"
+	).isLength({ min: 6 }),
 	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
@@ -32,16 +30,14 @@ router.post(
 		const { name, email, password } = req.body;
 
 		try {
-			// check if user exists
 			let user = await User.findOne({ email });
 
 			if (user) {
-				return res.status(400).json({
-					errors: [{ msg: "User already exists" }],
-				});
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "User already exists" }] });
 			}
 
-			// get users gravatar
 			const avatar = normalize(
 				gravatar.url(email, {
 					s: "200",
@@ -58,17 +54,18 @@ router.post(
 				password,
 			});
 
-			// encript user password
 			const salt = await bcrypt.genSalt(10);
+
 			user.password = await bcrypt.hash(password, salt);
+
 			await user.save();
 
-			// return jsonwebtoken
 			const payload = {
 				user: {
 					id: user.id,
 				},
 			};
+
 			jwt.sign(
 				payload,
 				config.get("jwtSecret"),
@@ -78,7 +75,6 @@ router.post(
 					res.json({ token });
 				}
 			);
-
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send("Server error");
