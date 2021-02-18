68e6efea4467f513af2b2aaa568ca049df76f1df
Bill Li <billli@gmail.com>
2021-02-18 17:49:04 -0500

    reformated files using prettier

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,78 +1,78 @@
-import api from '../utils/api';
-import { setAlert } from './alert';
+import api from "../utils/api";
+import { setAlert } from "./alert";
 import {
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
-  USER_LOADED,
-  AUTH_ERROR,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  LOGOUT
-} from './types';
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOGOUT,
+} from "./types";
 
 // Load User
-export const loadUser = () => async dispatch => {
-  try {
-    const res = await api.get('/auth');
+export const loadUser = () => async (dispatch) => {
+	try {
+		const res = await api.get("/auth");
 
-    dispatch({
-      type: USER_LOADED,
-      payload: res.data
-    });
-  } catch (err) {
-    dispatch({
-      type: AUTH_ERROR
-    });
-  }
+		dispatch({
+			type: USER_LOADED,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: AUTH_ERROR,
+		});
+	}
 };
 
 // Register User
-export const register = formData => async dispatch => {
-  try {
-    const res = await api.post('/users', formData);
+export const register = (formData) => async (dispatch) => {
+	try {
+		const res = await api.post("/users", formData);
 
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data
-    });
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
+		dispatch({
+			type: REGISTER_SUCCESS,
+			payload: res.data,
+		});
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
 
-    if (errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-    }
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
 
-    dispatch({
-      type: REGISTER_FAIL
-    });
-  }
+		dispatch({
+			type: REGISTER_FAIL,
+		});
+	}
 };
 
 // Login User
-export const login = (email, password) => async dispatch => {
-  const body = { email, password };
+export const login = (email, password) => async (dispatch) => {
+	const body = { email, password };
 
-  try {
-    const res = await api.post('/auth', body);
+	try {
+		const res = await api.post("/auth", body);
 
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data
-    });
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: res.data,
+		});
 
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
 
-    if (errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-    }
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
 
-    dispatch({
-      type: LOGIN_FAIL
-    });
-  }
+		dispatch({
+			type: LOGIN_FAIL,
+		});
+	}
 };
 
 // Logout
--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-import setAuthToken from './utils/setAuthToken';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import setAuthToken from "./utils/setAuthToken";
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+	rootReducer,
+	initialState,
+	composeWithDevTools(applyMiddleware(...middleware))
 );
 
 // set up a store subscription listener
@@ -22,14 +22,14 @@ const store = createStore(
 let currentState = store.getState();
 
 store.subscribe(() => {
-  // keep track of the previous and current state to compare changes
-  let previousState = currentState;
-  currentState = store.getState();
-  // if the token changes set the value in localStorage and axios headers
-  if (previousState.auth.token !== currentState.auth.token) {
-    const token = currentState.auth.token;
-    setAuthToken(token);
-  }
+	// keep track of the previous and current state to compare changes
+	let previousState = currentState;
+	currentState = store.getState();
+	// if the token changes set the value in localStorage and axios headers
+	if (previousState.auth.token !== currentState.auth.token) {
+		const token = currentState.auth.token;
+		setAuthToken(token);
+	}
 });
 
 export default store;
